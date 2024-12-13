ce102c494cbbc78bf9610839acf6304b3294b659
KimmyXYC <69386744+KimmyXYC@users.noreply.github.com>
2024-12-13 15:52:35 +0800

    refine: import

--- app/event.py
+++ app/event.py
@@ -8,6 +8,7 @@ import json
 import tempfile
 import time
 import os
+import re
 import xml.etree.ElementTree as ET
 from loguru import logger
 from datetime import datetime, timezone
@@ -15,7 +16,6 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding, ec
-import re
 
 
 async def load_from_url():
